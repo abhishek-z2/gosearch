a2cd7eed3051d67a20517eda70b5dd9f14f97b1d
Abhishek <145745124+abhishek-z2@users.noreply.github.com>
2026-07-29 15:24:36 +0530

    parse command line arguments

--- main.go
+++ main.go
@@ -6,5 +6,12 @@ import (
 )
 
 func main() {
-	fmt.Println(os.Args)
+	//fmt.Println(os.Args)
+	if len(os.Args) < 3 {
+		fmt.Println("not enough arguments")
+		return
+	}
+	query := os.Args[1]
+	file := os.Args[2]
+	fmt.Printf("query is %s , file is %s", query, file)
 }
