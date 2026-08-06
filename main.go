f37a2a6d9b38a0613a21c91a9cfcbf8b1d3e28b2
Abhishek <145745124+abhishek-z2@users.noreply.github.com>
2026-08-06 20:28:21 +0530

    refactor file search and error handling

--- main.go
+++ main.go
@@ -15,23 +15,49 @@ func main() {
 	query := os.Args[1]
 	file := os.Args[2]
 
-	filehandle, err := os.Open(file)
+	//checks if its a dir or a file
+	info, err := os.Stat(file)
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
+	} else {
+		fmt.Println("File Info: ", info.Name())
+	}
+
+	//searches the file
+	err = searchFile(file, query)
+	if err != nil {
+		fmt.Println("Error searching the file: ", err)
+		return
+	}
+
+}
+func searchFile(file, query string) error {
+	fmt.Println(file, query)
+	filehandle, err := os.Open(file)
+	if err != nil {
+		return err
 	}
 	defer filehandle.Close()
 
 	scanner := bufio.NewScanner(filehandle)
 	ln := 0
+	found := 0
 	for scanner.Scan() {
 		ln++
 		if line := scanner.Text(); strings.Contains(line, query) {
 			fmt.Printf("%v: %v\n", ln, line)
+			found++
 		}
+	}
+	if found == 0 {
+		fmt.Println("no matches found")
+	} else {
+		fmt.Printf("%d matches found", found)
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error reading file:", err)
+		return err
 	}
-
+	return nil
 }
